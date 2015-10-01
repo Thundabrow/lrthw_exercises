03aa338a13e95518295d95465d85af105292c5f1
David Jarrett <dave.jarrett@gmail.com>
2015-09-30 19:21:30 -0600

    Initial commit [#104389544]

---
+++ chapter_1/ex1.rb
@@ -0,0 +1,13 @@
+# puts "Hello World!"
+puts "Hello again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay!  Printing. "
+puts "I'd much rather you 'not'. "
+puts 'I "said" do not touch this.'
+puts "I am an additional line."
+
+# It appears that the # sign is the character to initiate a comment.
+
+
+
