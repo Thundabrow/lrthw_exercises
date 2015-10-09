8c9982ca9700bba80c726b78d353517a63ae8dd3
David Jarrett <dave.jarrett@gmail.com>
2015-10-08 22:24:52 -0600

    Created ex3_floating.rb [#104389548]

---
+++ chapter_3/ex3_floating.rb
@@ -0,0 +1,34 @@
+# Simple print statement.
+puts 'I will now count my chickens:'
+
+# Calculate number of hens.
+puts "Hens #{25.0 + 30.0 / 6.0}"
+# Calculate number of roosters.
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+
+# Simple print statement.
+puts "Now I will count the eggs:"
+
+# Print the answer to the equation below, using standard order of operations.
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+# Simple print statement.
+puts 'Is it true that 3 + 2 < 5 - 7?'
+
+# Print the boolean truth value of the statement below.
+puts 3.0 + 2.0 < 5.0 - 7.0
+
+# Print the questions and answers to the equations below.
+puts "What is 3 + 2?  #{3.0 + 2.0}"
+puts "What is 5 - 7? #{5.0 - 7.0}"
+
+# Simple print statement.
+puts "Oh, that’s why it’s false."
+
+# Simple print statement.
+puts "How about some more."
+
+# Print the questions and the answers, in the form of boolean truth values for the statements below.
+puts "Is it greater?  #{5.0 > -2.0}"
+puts "Is it greater or equal?  #{5.0 >= -2.0}"
+puts "Is it less or equal?  #{5.0 <= -2.0}"
